f0f78af6cf095e66c0b5c8475e2cecbd12363da1
eleeisb <elee@systemsbiology.org>
2024-08-07 23:06:18 -0700

    BQ_PROJECT_NAMES delimiter changed

--- cl_functions/src/main.py
+++ cl_functions/src/main.py
@@ -1,9 +1,4 @@
-# import pymysql
-# from fn_daily_management import daily_management
-# from fn_account_approval import account_approval
 from fn_bq_search_metadata import run_bq_metadata_etl
-# CTB_CL_FN_DAILY_MANAGEMENT = 0
-# CTB_CL_FN_ACCOUNT_APPROVAL = 1
 
 if __name__ == "__main__":
     run_bq_metadata_etl(None)
